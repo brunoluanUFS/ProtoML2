fe80ca229809d2bcf8b03efb422d3b9282fc7151
BrunoLuanUFS <bruno.leite@dcomp.ufs.br>
2021-11-03 15:44:06 -0300

    dia 2 - 2

--- app.py
+++ app.py
@@ -39,24 +39,24 @@ async def Recebe_CSV_Gera_Relatorio(file: UploadFile = File(...)):
 @app.post("/TreinaClassificador/")
 async def Treina_Classificador(target: str = Form(...)):
 
+    from sklearn import svm
     from sklearn.naive_bayes import GaussianNB
 
     df = pd.read_csv("dataset.csv")
     
     X = df.loc[:, df.columns != target]
-    Y = df.loc[:, df.columns == target]
-
-    atributos = str(X.columns.values.tolist())
+    y = df.loc[:, df.columns == target]
 
     clf = GaussianNB()
-    clf.fit(X,Y)    # Saving the model to a serialized .pkl file
-    
+    clf.fit(X,y.values.ravel())
+    score = str(round(clf.score(X,y)*100,2))+"%"
 
-    pkl_filename = "../clf_model.pkl"
+    pkl_filename = "clf_model.pkl"
     with open(pkl_filename, 'wb') as file:
         pickle.dump(clf, file)
-
-    return f"O modelo foi treinado com GaussianNB, com atributos {str(atributos)} e target {target}"
+        
+    atributos = str(X.columns.values.tolist())
+    return f"O modelo foi treinado com atributos: {str(atributos)}, target: {target} e {score} de acurácia média"
 
 @app.post('/InferenciaClassificador/')
 async def predict(atributos: list = Query([])):
@@ -66,49 +66,10 @@ async def predict(atributos: list = Query([])):
 
     atributos = pd.DataFrame(lista)
 
-    pkl_filename = "../clf_model.pkl"
+    pkl_filename = "clf_model.pkl"
     with open(pkl_filename, 'rb') as file:
         clf = pickle.load(file)
 
     pred = clf.predict(atributos)[0]
-    prob = clf.pre
 
     return pred
-
-# from pydantic import BaseModel
-
-# # Creating class to define the request body and the type hints of each attribute
-# class request_body(BaseModel):
-# 	sepal_length : float
-# 	sepal_width : float
-# 	petal_length : float
-# 	petal_width : float
-
-# # Loading Iris Dataset
-# iris = load_iris()
-
-# # Getting our Features and Targets
-# X = iris.data
-# Y = iris.target
-
-# # Creating and Fitting our Model
-# clf = GaussianNB()
-# clf.fit(X,Y)
-
-# # Creating an Endpoint to receive the data
-# # to make prediction on.
-# @app.post('/predict')
-# def predict(data : request_body):
-# 	# Making the data in a form suitable for prediction
-# 	test_data = [[
-# 			data.sepal_length,
-# 			data.sepal_width,
-# 			data.petal_length,
-# 			data.petal_width
-# 	]]
-	
-# 	# Predicting the Class
-# 	class_idx = clf.predict(test_data)[0]
-	
-# 	# Return the Result
-#	return { 'class' : iris.target_names[class_idx]}
