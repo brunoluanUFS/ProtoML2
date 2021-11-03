7bf5306524a1525c25a05a70c8900a9155f5fff1
BrunoLuanUFS <bruno.leite@dcomp.ufs.br>
2021-11-03 00:07:02 -0300

    ok

--- app.py
+++ app.py
@@ -20,4 +20,21 @@ async def create_upload_file(file: UploadFile = File(...)):
     analise.to_file(f"analise_{data}.html")
     FileResponse(f"analise_{data}.html")
 
-    return "Análise Executada"
+    return "Análise Executada - tratatrtrartatratratra"
+
+
+
+#     from fastapi import FastAPI, File, Form, UploadFile
+
+# app = FastAPI()
+
+
+# @app.post("/files/")
+# async def create_file(
+#     file: bytes = File(...), fileb: UploadFile = File(...), token: str = Form(...)
+# ):
+#     return {
+#         "file_size": len(file),
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#     }
