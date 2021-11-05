d46b8648b97250262beb96d748924fa40db56020
BrunoLuanUFS <bruno.leite@dcomp.ufs.br>
2021-11-05 08:20:37 -0300

    começando a mexer no deploy

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
+import streamlit as st
 from fastapi import FastAPI, File, Form, UploadFile, Query
 import shutil
-from fastapi.param_functions import Query
-from numpy.core.fromnumeric import size
 import pandas as pd
-from pandas.core.reshape.melt import lreshape
 from pandas_profiling import ProfileReport
 from fastapi.responses import FileResponse
 from datetime import datetime
 import pickle
-from typing import List
-
 import numpy as np
 
 app = FastAPI()
@@ -22,7 +18,7 @@ async def Upload_CSV(file: UploadFile = File(...)):
     df = pd.read_csv("dataset.csv")
     colunas = str(df.columns.values.tolist())
 
-    return f"O dataset foi carregado e possui as colunas {colunas}"
+    return f"O dataset foi carregado e possui {len(df.columns.values.tolist())} colunas {colunas}"
 
 
 @app.post("/Analisador/")
@@ -58,28 +54,35 @@ async def Treina_Classificador(target: str = Form(...)):
         pickle.dump(GNB, file)
         
     atributos = str(X.columns.values.tolist())
-    return f"O modelo foi treinado com atributos: {str(atributos)}, target: {target} e {score} de acurácia média"
+    return f"O modelo foi treinado e salvo no servidor com atributos: {str(atributos)}, target: {target} e {score} de acurácia média"
 
 @app.post("/TreinaClassificador-LogisticRegression/")
 async def Treina_Classificador(target: str = Form(...)):
 
     from sklearn.linear_model import LogisticRegression
-
     df = pd.read_csv("dataset.csv")
+    # PREPROCESSAMENTO
+
+    # df_numeric = df._get_numeric_data()
+    # print(df_numeric)
+    # cols = df.columns
+    # num_cols = df._get_numeric_data().columns
+    # cat_cols = list(set(cols) - set(num_cols))
+
+    # ONEHOT
     
+
+    # TREINA O MODELO
     X = df.loc[:, df.columns != target]
     y = df.loc[:, df.columns == target]
-
     LR = LogisticRegression()
     LR.fit(X,y.values.ravel())
     score = str(round(LR.score(X,y)*100,2))+"%"
-
     pkl_filename = "LR_model.pkl"
     with open(pkl_filename, 'wb') as file:
         pickle.dump(LR, file)
-        
     atributos = str(X.columns.values.tolist())
-    return f"O modelo foi treinado com atributos: {str(atributos)}, target: {target} e {score} de acurácia média"
+    return f"O modelo foi treinado e salvo no servidor com atributos: {str(atributos)}, target: {target} e {score} de acurácia média"
 
 @app.post('/InferenciaGNB/')
 async def predictGNB(q: list = Query([])):
@@ -100,22 +103,14 @@ async def predictLR(q: list = Query([])):
     for i in q:
         q2.append(np.float_(i))
 
-    from sklearn.linear_model import LogisticRegression
-
-    df = pd.read_csv("dataset.csv")
-
-    target = "class"
-    X = df.loc[:, df.columns != target]
-    y = df.loc[:, df.columns == target]
-
-    LR = LogisticRegression()
-    LR.fit(X,y.values.ravel())
-
-    # pkl_filename = "LR_model.pkl"
-    # with open(pkl_filename, 'rb') as file:
-    #     LR = pickle.load(file)
+    pkl_filename = "LR_model.pkl"
+    with open(pkl_filename, 'rb') as file:
+        LR = pickle.load(file)
     
     print(q,[q2],len(q),len(q2))
 
     pred = LR.predict([q2])
     return str(pred)
+
+
+### PARTE DO HTML
